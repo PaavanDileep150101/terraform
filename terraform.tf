e5d435e0ebeb978d41ba5eb5c5454dfca37a66dc
PaavanDileep150101 <dileep.apd123@gmail.com>
2023-04-20 23:25:33 +0530

    terraform file

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIA5TMEOCTKGT56MZG3"
+secret_key = "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"
+}
+
+
+resource "aws_instance" "web" {
+ami     = "ami-007855ac798b5175e"
+instance_type = "t2.micro"
+key_name     = "passwd"
+tags  =  {
+Name  = "terraformusingpipeline"
+}
+
+}
